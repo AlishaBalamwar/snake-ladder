12772ec863bf0b7feab8991b9c8b298894d06333
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-23 23:54:30 +0530

    modified uc3

--- uc3.java
+++ uc3.java
@@ -1,6 +1,7 @@
 package snake;
 
 public class uc3 {
+	public static void main(String[] args) {
 	int position=43;
 	int snake= (int) Math.floor(Math.random() * 10) %3;
 	int check= (int) Math.floor(Math.random() * 10) %6 +1;
