965755b3e39f58dbaf3385f8c39b7fc05511f95f
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-08-23 23:17:34 +0530

    player rolls the die to get number

---
+++ uc2.java
@@ -0,0 +1,10 @@
+package snake;
+
+public class uc2 {
+public static void main(String[] args) {
+		
+		int check =(int) Math.floor(Math.random() * 10) %6 +1;
+		
+		System.out.println("Current position is :" + check);
+	}
+}
